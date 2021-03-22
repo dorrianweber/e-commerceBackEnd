7da621a936631fc1dc6d3a600de7240034020474
Dorrian Weber <dorrianweber@gmail.com>
2021-03-22 14:08:42 -0700

    Took a stab at product-routes.js

--- models/index.js
+++ models/index.js
@@ -17,12 +17,20 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tagged_products',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tags',
 });
 
 module.exports = {
